d881aa3ea195e0cbfdd032932724e3073b6e2652
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2025-05-29 14:31:53 +0300

    feat: adds simple pagination

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -1,10 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+// set cameraid to the last used camera
+const cameraId = ref(1);
+</script>
 
 <template>
   <header
     class="h-16 px-6 flex items-center justify-between border-b border-gray-200 bg-white dark:bg-gray-950 dark:border-gray-800"
   >
-    <!-- Header content here -->
     <p class="text-lg font-semibold">Dashboard Header</p>
+
+    <UButton class="font-bold rounded-full" icon="i-lucide-check"
+      >All cameras annotated</UButton
+    >
+    <div>
+      <UPagination v-model:page="cameraId" :items-per-page="1" :total="2" />
+    </div>
   </header>
 </template>
