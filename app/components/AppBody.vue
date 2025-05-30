25aaca0f2d3e74701a76b2b67d00121633e0d870
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2025-05-30 19:58:24 +0300

    feat: use Image uploader

--- app/components/AppBody.vue
+++ app/components/AppBody.vue
@@ -1,7 +1,5 @@
-<script setup lang="ts"></script>
+<script setup></script>
 
 <template>
-  <main class="flex-1 p-6 overflow-auto bg-gray-50 dark:bg-gray-950">
-    <p>Main dashboard content goes here.</p>
-  </main>
+  <ImageUploader />
 </template>
