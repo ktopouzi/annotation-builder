5ff715282715f25289744cdd420336fa0f21926c
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2025-05-31 16:09:36 +0300

    chore: updates sidebar

--- app/components/AppSidebar.vue
+++ app/components/AppSidebar.vue
@@ -1,36 +1,10 @@
-<script setup lang="ts">
-const { addCamera, isEditing } = useCameras();
-
-function handleAddCamera() {
-  addCamera({
-    id: Date.now(),
-    name: "New Camera",
-    description: "New camera description",
-    annotated: false,
-    annotations: [],
-  });
-}
-</script>
+<script setup></script>
 
 <template>
   <aside
-    class="w-64 bg-gray-100 border-r border-gray-200 dark:bg-gray-900 dark:border-gray-800"
+    class="w-64 bg-gray-100 border-r border-gray-200 dark:bg-gray-900 dark:border-gray-800 hidden md:block"
   >
     <div class="p-4">
-      <div class="flex items-center justify-between">
-        <p class="text-lg font-bold">Camera List</p>
-
-        <UButton
-          class="font-bold rounded-full cursor-pointer"
-          icon="i-lucide-plus"
-          @click="handleAddCamera"
-        />
-        <UButton
-          class="font-bold rounded-full"
-          icon="i-lucide-edit"
-          @click.stop="isEditing = true"
-        />
-      </div>
       <CameraList />
     </div>
   </aside>
