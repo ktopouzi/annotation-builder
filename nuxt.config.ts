591c4a90406307aaa7ba394563d30ae3cb8a673d
Kimon Topouzidis <kimon.topouzidis@gmail.com>
2025-06-02 15:32:13 +0300

    feat: adds nuxt tests

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
 
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/test-utils/module"],
 
   css: ["~/assets/css/main.css"],
 
